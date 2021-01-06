2ca1184d2fd13ad4ac6e3afdb6043696095538ce
Alanne Soares <alannesilva@yahoo.com.br>
2021-01-05 21:23:10 -0300

    Adicionando o primeiro comentário.

--- src/main/java/br/com/hello/demo/DemoApplication.java
+++ src/main/java/br/com/hello/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// esta é a classe principal
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
